d408a19d67a9a9b9daf2c3bcdf99e4c4a003dbe4
josnkina-25 <josnkina@icloud.com>
2026-01-20 21:56:04 -0500

    Fix getLearnerData implementation and calculations

--- index.js
+++ index.js
@@ -1,3 +1,81 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript"
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
+
 function getLearnerData(courseInfo, assignmentGroup, learnerSubmission) {
   try {
     if (courseInfo.id !== assignmentGroup.course_id) {
@@ -18,7 +96,7 @@ function getLearnerData(courseInfo, assignmentGroup, learnerSubmission) {
       let totalScore = 0;
       let totalPoints = 0;
 
-      for (let assignment of assignmentGroup.assignemts) {
+      for (let assignment of assignmentGroup.assignments) {
         const dueDate = new Date(assignment.due_at);
 
         if (dueDate > today) continue;
@@ -52,3 +130,10 @@ function getLearnerData(courseInfo, assignmentGroup, learnerSubmission) {
     return [];
   }
 }
+const result = getLearnerData(
+  CourseInfo,
+  AssignmentGroup,
+  LearnerSubmissions);
+  console.log(result);
+
+
